cbf57a6d9f471eecf5a08ffab290c2e19954a6a8
Jesse Smick <janka102@gmail.com>
2017-12-02 17:57:39 -0800

    Change Resume style

--- resume.php
+++ resume.php
@@ -7,27 +7,25 @@
 
 <div class="row">
   <div class="col-12">
-    <h1>Resume</h1>
-
     <section>
       <h2>Experience</h2>
 
       <article>
-        <h3>Acutulus / <small>Jun 2017 - Present</small></h3>
-        <p>Web Developer Intern</p>
+        <h3>Acutulus / <small>Web Developer Intern</small></h3>
+        <p>Jun 2017 - Present</p>
         <p>Worked on their server framework to improve and add new features. Improved a client's website admin interface</p>
       </article>
 
       <article>
-        <h3>Bay Computing Group / <small>Jun 2015 - Jul 2017</small></h3>
-        <p>Helpdesk Technician</p>
+        <h3>Bay Computing Group / <small>Helpdesk Technician</small></h3>
+        <p>Jun 2015 - Jul 2017</p>
         <p>Provided Tier 1 help desk support to a client with thousands of employees. Created and managed tickets from phone calls
           and emails.</p>
       </article>
 
       <article>
-        <h3>Christiansen Fasteners / <small>Jul 2014 - Sep 2014</small></h3>
-        <p>Web Developer</p>
+        <h3>Christiansen Fasteners / <small>Web Developer</small></h3>
+        <p>Jul 2014 - Sep 2014</p>
         <p>Co-created their website from the ground up, including hundreds of product pages. Scraped product data and built the
           website.
         </p>
@@ -52,13 +50,13 @@
       <h2>Education</h2>
 
       <article>
-        <h3>San Francisco State University / <small>Jun 2017 - Present</small></h3>
-        <p>Bachelors of Science in Computer Science</p>
+        <h3>San Francisco State University / <small>B.S., Computer Science</small></h3>
+        <p>Jun 2017 - Present</p>
       </article>
 
       <article>
-        <h3>Diablo Valley College / <small>Jun 2014 - Aug 2017</small></h3>
-        <p>Associates of Science in Computer Science</p>
+        <h3>Diablo Valley College / <small>A.S., Computer Science</small></h3>
+        <p>Jun 2014 - Aug 2017</p>
       </article>
     </section>
   </div>
