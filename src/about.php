894175827e9ca93cf50c7803c3e2960fa6b86888
Jesse Smick <janka102@gmail.com>
2018-02-03 01:03:50 -0800

    Fix whitespace

--- src/about.php
+++ src/about.php
@@ -9,7 +9,7 @@
   <div class="col-12">
     <h1>About Me</h1>
     <p>My name is Jesse Smick. I have lived my whole life in the East Bay Area moving around between a few cities.</p>
-    <p> I took a computer programming class in middle school and have enjoyed programming ever since. I started out with very simple web pages using vertical <code>&lt;marquee&gt;</code> tags everywhere.</p>
+    <p>I took a computer programming class in middle school and have enjoyed programming ever since. I started out with very simple web pages using vertical <code>&lt;marquee&gt;</code> tags everywhere.</p>
     <p>I went to Diablo Valley College after high school. I am currently a transfer student to San Francisco State University.</p>
     <p>I am taking CSC 412 in the Fall of 2017.</p>
   </div>
